d68c797023ad061ba951ff619f56497130951167
Gordon Caister <gordoncaister@gmail.com>
2020-05-15 10:29:13 -0500

    made correction to seed

--- data/seeds/001_initial.js
+++ data/seeds/001_initial.js
@@ -8,17 +8,17 @@ exports.seed = function(knex) {
         {
           project_name: "Node DB Data Persistence",
           project_description:"Sprint challenge for Node API Data Persistence",
-          completed: false
+          project_status: false
         },
         {
           project_name: "Fix bed box spring",
           project_description:"Spring steel in box spring is bent due to jumping on bed, needs to be rebent and wood replaced",
-          completed: false
+          project_status: false
         },
         {
           project_name: "Build Bicycle",
           project_description:"Buy parts and build a bicycle",
-          completed: true
+          project_status: true
         }
       ]);
     });
