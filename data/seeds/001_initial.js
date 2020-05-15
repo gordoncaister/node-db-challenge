53643bd7f59a11c68282d07fb5ad4db0ab862eb2
Gordon Caister <gordoncaister@gmail.com>
2020-05-15 10:28:31 -0500

    created initial seed

--- data/seeds/001_initial.js
+++ data/seeds/001_initial.js
@@ -1,13 +1,25 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('projects').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('projects').insert([
+        {
+          project_name: "Node DB Data Persistence",
+          project_description:"Sprint challenge for Node API Data Persistence",
+          completed: false
+        },
+        {
+          project_name: "Fix bed box spring",
+          project_description:"Spring steel in box spring is bent due to jumping on bed, needs to be rebent and wood replaced",
+          completed: false
+        },
+        {
+          project_name: "Build Bicycle",
+          project_description:"Buy parts and build a bicycle",
+          completed: true
+        }
       ]);
     });
 };
